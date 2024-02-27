159f45995fcfc7a2c989b709b45a74a0580c920b
Francesco Zatti <francesco.zatti98gmail.com>
2024-02-27 12:02:19 +0100

    AGGIORNAMENTO FINALE: QUESTA VOLTA NELLA CARTELLA GIUSTA

--- header.php
+++ header.php
@@ -9,6 +9,16 @@
     </title>
     <link rel="stylesheet" href="<?php echo esc_url(get_stylesheet_uri() . '?ver=' . time()); ?>" type="text/css" />
     <?php wp_head(); ?>
+	  <style>
+    .navbar-light .navbar-toggler {
+        background-color: transparent !important;
+		border: none !important;
+    }
+		  
+		  .navbar .navbar-toggler-icon {
+  background-image: url("data:image/svg+xml;charset=utf8,%3Csvg viewBox='0 0 30 30' xmlns='http://www.w3.org/2000/svg'><path stroke='rgba(255, 255, 255, 1)' stroke-width='2' stroke-linecap='round' stroke-miterlimit='10' d='M4 7h22M4 15h22M4 23h22'/%3E%3C/svg%3E");
+}
+    </style>
 </head>
 
 <body <?php body_class(); ?>>
@@ -17,10 +27,10 @@
             <a class="navbar-brand" href="<?php echo esc_url(home_url('/')); ?>">
                 <?php echo esc_html(get_bloginfo('name')); ?>
             </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+<button class="navbar-toggler text-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+</button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ml-auto">
                     <?php
